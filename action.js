4e370d847db8a7a3a46e4845b7d9bb0d006c3936
Ram-Goutham <brgowtham97@gmail.com>
2021-05-31 01:30:44 +0530

    --- Auto-Git Commit ---

--- action.js
+++ action.js
@@ -1,23 +1,12 @@
 let spinner = document.querySelector(".spinner");
 let toggle = document.querySelector(".toggle");
 let toggle__btn = document.querySelector(".toggle__btn");
+let chow = 0;
+let Button = document.querySelector(".Button");
 
 
 function myFunction(){
-//     spinner.classList.toggle("block");
-//     toggle__btn.classList.toggle("none");
-    let spin = spinner.style.display;
-    let togg = toggle__btn.style.display;
-    if(spin == "block" && togg == "none"){
-        spinner.style.display = "none";
-        toggle__btn.style.display = "block";
-        console.log("chow",spin,togg);
-        
-    }
-    else{
-        spinner.style.display = "block";
-        toggle__btn.style.display = "none";
-        console.log("ram");
-    }
-    console.log(spin,togg);
-}
+    spinner.classList.toggle("block");
+    toggle__btn.classList.toggle("none");
+};
+
