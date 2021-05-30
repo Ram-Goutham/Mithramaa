a64632c763c6c35af4bddc14f17f9d842a3b77cd
Ram-Goutham <brgowtham97@gmail.com>
2021-05-31 02:31:26 +0530

    --- Auto-Git Commit ---

--- action.js
+++ action.js
@@ -3,13 +3,22 @@ let toggle = document.querySelector(".toggle");
 let toggleBtn = document.querySelector(".toggle__btn");
 let chow = 0;
 let Button = document.querySelector(".Button");
-let menuToggle = document.querySelector(".menu__toggle")
+let menuToggle = document.querySelector(".menu__toggle");
+let element = document.querySelector('.menu__list','::after');
+console.log(element);
 
 function myFunction(){
     spinner.classList.toggle("block");
     toggleBtn.classList.toggle("none");
     menuToggle.classList.toggle("show");
 };
+element.addEventListener("click",()=>{
+    spinner.classList.toggle("block");
+    toggleBtn.classList.toggle("none");
+    menuToggle.classList.toggle("show");
+    console.log("chow");
+});
+
 
 
 
