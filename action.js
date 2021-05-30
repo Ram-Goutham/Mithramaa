e7150da4cb62f48879ef427e95f88ccce2d41353
Ram-Goutham <brgowtham97@gmail.com>
2021-05-30 05:54:13 +0530

    --- Auto-Git Commit ---

--- action.js
+++ action.js
@@ -1,3 +1,7 @@
 let spinner = document.querySelector(".spinner");
 let toggle = document.querySelector(".toggle");
 let toggle__btn = document.querySelector(".toggle__btn");
+function myFunction() = toggle.addEventListener("toggle",()=>{
+    spinner = "display:block";
+    toggle__btn = "display:none";
+})
